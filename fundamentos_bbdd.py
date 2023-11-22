f3384e2dd19fe1a9fabb358c8448c80d3a02888b
José Carlos Del Valle <30965492+jcval94@users.noreply.github.com>
2023-11-21 21:08:44 -0600

    Update fundamentos_bbdd.py
    link sencillo

--- fundamentos_bbdd.py
+++ fundamentos_bbdd.py
@@ -78,7 +78,7 @@ def main():
     elif selected_tab == "BBDD":
         # Botón de descarga directa del diccionario de datos en el cuerpo principal
         st.markdown(
-            f'<h3 href="{url_diccionario_datos}" download="Diccionario_de_datos.xlsx">📥 Descargar Diccionario de Datos</h3>',
+            f'<a href="{url_diccionario_datos}" download="Diccionario_de_datos.xlsx">📥 Descargar Diccionario de Datos</a>',
             unsafe_allow_html=True
         )
 
